3a2c3741af1cb0b2ad16e95c546d1cce452866c9
David Fertitta <Davidj.fertitta@gmail.com>
2023-07-31 21:34:19 -0600

    tailwind shit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkmode: false,
     theme: {
+        extend: {},
+    },
+    variants: {
         extend: {},
     },
     plugins: [],
